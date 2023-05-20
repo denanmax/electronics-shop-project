e1db218a48a2a32b4d6d2637153a7199700ade9f
denanmax <deshs93@gmail.com>
2023-05-20 15:02:13 +0200

    tests with new functions added

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-
 from src.item import Item
+import pytest
 
 
 def test_init():
@@ -12,6 +12,9 @@ def test_init():
     # проверка при нулево стоке
     item = Item("Дискошар", 10.0, 0)
     assert item.calculate_total_price() == 0
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
 
 
 def test_calculate_total_price():
@@ -25,3 +28,14 @@ def test_apply_discount():
     item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 1600
+
+
+def test_name():
+    item = Item('Телефон', 10000, 5)
+    item.name = "Смартфон"
+    assert item.name == "Смартфон"
+
+def test_name_setter():
+    item = Item('Квадракоптер', 10000, 5)
+    with pytest.raises(ValueError):
+        item.name = "Название товара не должно быть больше 10 символов"
