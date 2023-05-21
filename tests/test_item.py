d4a6af77ae8bb0b33a69a4e41693fa618c43a2e8
denanmax <deshs93@gmail.com>
2023-05-21 23:02:57 +0200

    new tests added

--- tests/test_item.py
+++ tests/test_item.py
@@ -5,8 +5,8 @@ import pytest
 
 def test_init():
     """Тесты проверки функции init"""
-    item = Item("Видеомагнитофон", 2000, 3)
-    assert item.name == "Видеомагнитофон"
+    item = Item("Микрофон", 2000, 3)
+    assert item.name == "Микрофон"
     assert item.price == 2000
     assert item.quantity == 3
     # проверка при нулево стоке
@@ -19,12 +19,12 @@ def test_init():
 
 def test_calculate_total_price():
     """Вывод общей суммы по количеству"""
-    assert Item("Автомагнитола", 1000, 5).calculate_total_price() == 5000
+    assert Item("Микрофон", 1000, 5).calculate_total_price() == 5000
 
 
 def test_apply_discount():
     """Вывод общей суммы при скидке"""
-    item = Item("DVD проигрыватель", 2000, 3)
+    item = Item("Микрофон", 2000, 3)
     item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 1600
@@ -35,7 +35,21 @@ def test_name():
     item.name = "Смартфон"
     assert item.name == "Смартфон"
 
+
 def test_name_setter():
-    item = Item('Квадракоптер', 10000, 5)
     with pytest.raises(ValueError):
-        item.name = "Название товара не должно быть больше 10 символов"
+        Item('Квадракоптер', 10000, 5)
+    item = Item('Дрон', 5000, 8)
+    assert item.name == 'Дрон'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert Item.instantiate_from_csv() is None
+    with pytest.raises(TypeError):
+        Item.instantiate_from_csv("/home/denis/PycharmProjects/electronics-shop-project/src/item.py")
+
+
+
+
+
