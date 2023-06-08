0e774f1e6b8fd566050153c84f1bc3a68b057e51
denanmax <deshis93@gmail.com>
2023-06-08 12:37:56 +0200

    corrected for proper testing

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,4 +1,10 @@
+import pathlib
+
 from src.item import Item
+from pathlib import Path
+
+dir_path = pathlib.Path.cwd()
+
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
@@ -6,5 +12,6 @@ if __name__ == '__main__':
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+    path_incorrect_file = Path(dir_path, '../src/test_item.csv')
+    Item.instantiate_from_csv(path_incorrect_file)
     # InstantiateCSVError: Файл item.csv поврежден
